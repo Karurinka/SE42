f4c96aecadc91f74dcde58516b5c19521f8ef481
larsvandriel <lars.van.driel@hotmail.com>
2018-05-15 11:03:33 +0200

    Fix

--- src/test/java/Assignment1Tests.java
+++ src/test/java/Assignment1Tests.java
@@ -19,7 +19,7 @@ public class Assignment1Tests
     private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("bankPU");
     private EntityManager em;
     private AccountDAOJPAImpl accountDAOJPA;
-
+ 
     @Before
     public void Before()
     {
